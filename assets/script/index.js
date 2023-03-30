aeaac560e9d349b7e04e3353f5e0a061b162fbc6
Noah Miller <Noseby@live.ca>
2023-03-30 11:09:40 -0500

    first commit

---
+++ assets/script/index.js
@@ -0,0 +1,44 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    Cookies assignment
+
+    Noah Miller
+
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+'use strict'
+
+const modal = document.querySelector('.modal');
+const content = document.querySelector('.content');
+const accept = document.querySelector('.accept');
+const settings = document.querySelector('.settings')
+const settingsModal = document.querySelector('.settingsModal');
+
+function openSettings() {
+    settingsModal.style.display = 'inline-block';
+    content.style.display = 'none';
+}
+
+
+
+function setCookie(name, value, options = {}) {
+    options = {
+      path: '/',
+      SameSite: 'Lax',
+      ...options
+    };
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == settingsModal) {
+      settingsModal.style.display = "none";
+    }
+}
