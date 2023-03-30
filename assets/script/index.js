1f274df1613e0480949732ef07269cdb02b6a78e
Noah Miller <Noseby@live.ca>
2023-03-30 13:03:14 -0500

    js start

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,22 +13,28 @@ const content = document.querySelector('.content');
 const accept = document.querySelector('.accept');
 const settings = document.querySelector('.settings')
 const settingsModal = document.querySelector('.settingsModal');
-const browser = 
+const browser = document.querySelector('.browser');
+const os = document.querySelector('.os');
+const sw = document.querySelector('.sw');
+const sh = document.querySelector('.sh');
 
 function openSettings() {
     settingsModal.style.display = 'inline-block';
     content.style.display = 'none';
 }
 
-
-
 function setCookie(name, value, options = {}) {
+    const date = new Date();
+    date.setSeconds(date.getSeconds() + 15);
+    let expires = "expires=" + date.toUTCString();
     options = {
       path: '/',
       SameSite: 'Lax',
       ...options
     };
 }
+
+
 
 
 // When the user clicks anywhere outside of the modal, function will close modal
